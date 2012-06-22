6ac1b3c405f6b768824c56968e6a553a96584322
Steve Kondik <shade@chemlab.org>
2012-06-21 22:14:41 -0700

    Initial import

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,57 @@
+# Copyright (C) 2009 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from common msm8960
+-include device/samsung/msm8960-common/BoardConfigCommon.mk
+
+# Kernel
+BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom user_debug=31
+BOARD_KERNEL_BASE           := 0x80200000
+BOARD_FORCE_RAMDISK_ADDRESS := 0x81500000
+BOARD_KERNEL_PAGESIZE       := 2048
+
+# FIXME: Overlay has an issue when playing video in landscape mode
+COMMON_GLOBAL_CFLAGS += -DQCOM_ROTATOR_KERNEL_FORMATS
+
+# MTP
+BOARD_MTP_DEVICE := "/dev/usb_mtp_gadget"
+
+# Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/d2-common/recovery/recovery_keys.c
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Disable initlogo, Samsungs framebuffer is weird
+TARGET_NO_INITLOGO := true
+
+# HAX
+#BOARD_USE_SAMSUNG_SEPARATEDSTREAM := true
+#BOARD_USES_LIBMEDIA_WITH_AUDIOPARAMETER := true
+
---
+++ d2-common.mk
@@ -0,0 +1,75 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+## The gps config appropriate for this device
+PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
+
+## (2) Also get non-open-source specific aspects if available
+$(call inherit-product-if-exists, vendor/samsung/d2-common/d2-common-vendor.mk)
+
+## overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/d2-common/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Audio configuration
+PRODUCT_COPY_FILES += \
+	device/samsung/d2-common/snd_soc_msm/snd_soc_msm:system/etc/snd_soc_msm/snd_soc_msm \
+	device/samsung/d2-common/snd_soc_msm/Call_DL_Rec:system/etc/snd_soc_msm/Call_DL_Rec \
+	device/samsung/d2-common/snd_soc_msm/Call_UL_DL_Rec:system/etc/snd_soc_msm/Call_UL_DL_Rec \
+	device/samsung/d2-common/snd_soc_msm/DSP_Loopback:system/etc/snd_soc_msm/DSP_Loopback \
+	device/samsung/d2-common/snd_soc_msm/HiFi:system/etc/snd_soc_msm/HiFi \
+	device/samsung/d2-common/snd_soc_msm/HiFi_Low_Power:system/etc/snd_soc_msm/HiFi_Low_Power \
+	device/samsung/d2-common/snd_soc_msm/HiFi_Rec:system/etc/snd_soc_msm/HiFi_Rec \
+	device/samsung/d2-common/snd_soc_msm/HiFi_Voice_Recognition:system/etc/snd_soc_msm/HiFi_Voice_Recognition \
+	device/samsung/d2-common/snd_soc_msm/Loopback:system/etc/snd_soc_msm/Loopback \
+	device/samsung/d2-common/snd_soc_msm/Video_Call:system/etc/snd_soc_msm/Video_Call \
+	device/samsung/d2-common/snd_soc_msm/Voice_Call1:system/etc/snd_soc_msm/Voice_Call1 \
+	device/samsung/d2-common/snd_soc_msm/VoIP1:system/etc/snd_soc_msm/VoIP1 \
+	device/samsung/d2-common/snd_soc_msm/VoIP2:system/etc/snd_soc_msm/VoIP2 \
+	device/samsung/d2-common/snd_soc_msm/VoIP3:system/etc/snd_soc_msm/VoIP3 \
+	device/samsung/d2-common/snd_soc_msm/VoIP4:system/etc/snd_soc_msm/VoIP4 \
+	device/samsung/d2-common/snd_soc_msm/VoIP5:system/etc/snd_soc_msm/VoIP5
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/d2-common/ramdisk/init.qcom.rc:root/init.qcom.rc \
+    device/samsung/d2-common/ramdisk/init.qcom.sh:root/init.qcom.sh \
+    device/samsung/d2-common/ramdisk/init.qcom.usb.sh:root/init.qcom.usb.sh \
+    device/samsung/d2-common/ramdisk/init.target.rc:root/init.target.rc \
+    device/samsung/d2-common/ramdisk/initlogo.rle:root/initlogo.rle \
+    device/samsung/d2-common/ramdisk/ueventd.rc:root/ueventd.rc \
+	device/samsung/d2-common/ramdisk/init.emmc.rc:root/init.emmc.rc \
+	device/samsung/d2-common/ramdisk/init.bt.rc:root/init.bt.rc \
+	device/samsung/d2-common/ramdisk/init.sensor.rc:root/init.sensor.rc
+
+# Vold configuration
+PRODUCT_COPY_FILES += \
+    device/samsung/d2-common/vold.fstab:system/etc/vold.fstab
+
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# common msm8960
+$(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
+
+$(call inherit-product, frameworks/base/build/phone-xhdpi-1024-dalvik-heap.mk)
+
