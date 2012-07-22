2856841b165974f19173fcd8e8e1c72b0694e276
Shareef Ali <shareefalis@gmail.com>
2012-07-22 15:54:14 -0500

    d2: Fix A2DP.
    Don't use ICS audio_policy.so.
    
    Change-Id: Ifabe117e8854a073d66f976076a8fffee3c52192

--- d2-common.mk
+++ d2-common.mk
@@ -46,7 +46,8 @@ PRODUCT_COPY_FILES += \
 	device/samsung/d2-common/snd_soc_msm/VoIP2:system/etc/snd_soc_msm/VoIP2 \
 	device/samsung/d2-common/snd_soc_msm/VoIP3:system/etc/snd_soc_msm/VoIP3 \
 	device/samsung/d2-common/snd_soc_msm/VoIP4:system/etc/snd_soc_msm/VoIP4 \
-	device/samsung/d2-common/snd_soc_msm/VoIP5:system/etc/snd_soc_msm/VoIP5
+	device/samsung/d2-common/snd_soc_msm/VoIP5:system/etc/snd_soc_msm/VoIP5 \
+	device/samsung/d2-common/audio_policy.conf:system/etc/audio_policy.conf
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
