891e995829a9712aa7c7a105d3e619ea9b4fe101
Shareef Ali <shareefalis@gmail.com>
2012-08-15 05:04:49 +0400

    Revert "d2-common: adds modified ics hostapd for jb hotspot wpa support"
    proper workaround is found
    
    This reverts commit 6305078fc45a70cc9a9e60c52aa1170e1f6ce723

--- d2-common.mk
+++ d2-common.mk
@@ -46,11 +46,7 @@ PRODUCT_COPY_FILES += \
 	device/samsung/d2-common/snd_soc_msm/VoIP2:system/etc/snd_soc_msm/VoIP2 \
 	device/samsung/d2-common/snd_soc_msm/VoIP3:system/etc/snd_soc_msm/VoIP3 \
 	device/samsung/d2-common/snd_soc_msm/VoIP4:system/etc/snd_soc_msm/VoIP4 \
-    device/samsung/d2-common/snd_soc_msm/VoIP5:system/etc/snd_soc_msm/VoIP5
-
-# modified ics hostapd for jb hotspot wpa support
-PRODUCT_COPY_FILES += \
-    device/samsung/d2-common/prebuilt/hostapd:system/bin/hostapd
+	device/samsung/d2-common/snd_soc_msm/VoIP5:system/etc/snd_soc_msm/VoIP5
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
