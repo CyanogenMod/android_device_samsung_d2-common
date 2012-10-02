f184d9a8791f490fb53a262281d376432a63fb70
invisiblek <dan.pasanen@gmail.com>
2012-10-01 21:56:47 -0500

    d2-common: enable skipnullaid
    enables this for us: http://review.cyanogenmod.com/#/c/24095/
    
    Change-Id: I8916c0278ad418263c29521edcf583b29a38907b

--- d2-common.mk
+++ d2-common.mk
@@ -98,7 +98,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     lpa.decode=false \
     lpa.use-stagefright=false \
     rild.libpath=/system/lib/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.ril.v3=skipnullaid
 
 # common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
