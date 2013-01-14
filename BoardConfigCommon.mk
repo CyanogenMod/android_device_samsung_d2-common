1a7763691ee07f42e1fcb6d7824617a0381db6b8
invisiblek <dan.pasanen@gmail.com>
2013-01-13 23:07:33 -0600

    d2: bluetooth: disable BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY
    Change-Id: I58ee218c1c83354e757b80a0abbc31ac24848fa0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,6 +56,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d2-common/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/d2-common/bluetooth/vnd_d2.txt
+BOARD_BLUETOOTH_USES_HCIATTACH_PROPERTY := false
 
 # Disable initlogo, Samsungs framebuffer is weird
 TARGET_NO_INITLOGO := true
