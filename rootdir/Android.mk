2ab6b25b900a308568386f9471c2237551abb69f
Steve Kondik <shade@chemlab.org>
2013-05-01 12:08:49 -0700

    d2: Relocate modem link scripts
     * Moving from msm8960-common
    
    Change-Id: Ib470cbbf87cac950701a4ae7ba29f08de8015308

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -48,3 +48,18 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.mdm_links.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.mdm_links.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.modem_links.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.modem_links.sh
+include $(BUILD_PREBUILT)
+
