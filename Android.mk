4fc5cd1fe291abba7f8880fe3bc2adfd9f8acd8f
Steve Kondik <shade@chemlab.org>
2013-04-30 21:36:50 -0700

    d2: Make guard makefile more specific
    Change-Id: If9d91f802ccc99448a00f0288bf4bc4ddcb4364e

--- Android.mk
+++ Android.mk
@@ -24,6 +24,8 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifneq ($(filter d2att d2cri d2mtr d2spr d2tmo d2usc d2vzw,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
 endif
 endif
