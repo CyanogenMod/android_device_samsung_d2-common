7247807435f912a3e7a716ab5d72315733f3f13b
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-05-01 23:25:18 +0200

    d2-common: Fix relocation pathways for KeyLayout and GPS
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    Change-Id: I53a0c054d3b42b5b60cfad4f112f402526860128

--- d2-common.mk
+++ d2-common.mk
@@ -38,13 +38,13 @@ PRODUCT_COPY_FILES += \
 
 # Keymaps
 PRODUCT_COPY_FILES += \
-       device/samsung/msm8960-common/keylayout/fsa9485.kl:system/usr/keylayout/fsa9485.kl \
-       device/samsung/msm8960-common/keylayout/msm8960-snd-card_Button_Jack.kl:system/usr/keylayout/msm8960-snd-card_Button_Ja
-       device/samsung/msm8960-common/keylayout/sec_key.kl:system/usr/keylayout/sec_key.kl \
-       device/samsung/msm8960-common/keylayout/sec_keys.kl:system/usr/keylayout/sec_keys.kl \
-       device/samsung/msm8960-common/keylayout/sec_powerkey.kl:system/usr/keylayout/sec_powerkey.kl \
-       device/samsung/msm8960-common/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
-       device/samsung/msm8960-common/keylayout/sii9234_rcp.kl:system/usr/keylayout/sii9234_rcp.kl
+       device/samsung/d2-common/keylayout/fsa9485.kl:system/usr/keylayout/fsa9485.kl \
+       device/samsung/d2-common/keylayout/msm8960-snd-card_Button_Jack.kl:system/usr/keylayout/msm8960-snd-card_Button_Jack.kl \
+       device/samsung/d2-common/keylayout/sec_key.kl:system/usr/keylayout/sec_key.kl \
+       device/samsung/d2-common/keylayout/sec_keys.kl:system/usr/keylayout/sec_keys.kl \
+       device/samsung/d2-common/keylayout/sec_powerkey.kl:system/usr/keylayout/sec_powerkey.kl \
+       device/samsung/d2-common/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
+       device/samsung/d2-common/keylayout/sii9234_rcp.kl:system/usr/keylayout/sii9234_rcp.kl
 
 # Ramdisk
 PRODUCT_PACKAGES += \
--- gps/libloc_api_50001/Android.mk
+++ gps/libloc_api_50001/Android.mk
@@ -87,7 +87,7 @@ LOCAL_CFLAGS += \
 
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
-    device/samsung/msm8960-common/gps/ulp/inc
+    device/samsung/d2-common/gps/ulp/inc
 
 LOCAL_PRELINK_MODULE := false
 
@@ -120,7 +120,7 @@ LOCAL_CFLAGS += \
 ## Includes
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
-    device/samsung/msm8960-common/gps/ulp/inc
+    device/samsung/d2-common/gps/ulp/inc
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
