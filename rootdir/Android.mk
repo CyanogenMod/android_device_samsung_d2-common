2baf15826ae7e550dd1c571b2b03ef1cbc81698a
David Daynard <nardholio@gmail.com>
2013-06-23 21:09:08 -0400

    ApexQ: fix tethering and bluetooth MAC addressing
    WCN3660 does not start tethering when /data/system/wpa_supplicant
    symlink exists.
    
    Bluetooth init script does not properly start with current
    configuration. Bluetooth does work but the MAC address never gets set.
    These changes bring ApexQ in line with other WCN3660 devices such as
    Ville, Mint, and Mako.
    
    Change-Id: Iacaf482e754d02d11381ec8c38a829ec27f73edd

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -49,23 +49,34 @@ LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := init.qcom.mdm_links.sh
+#LOCAL_MODULE_TAGS  := optional eng
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := etc/init.qcom.mdm_links.sh
+#include $(BUILD_PREBUILT)
+
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := init.qcom.modem_links.sh
+#LOCAL_MODULE_TAGS  := optional eng
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := etc/init.qcom.modem_links.sh
+#include $(BUILD_PREBUILT)
+
+ifneq ($(filter apexqtmo,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.mdm_links.sh
+LOCAL_MODULE       := init.qcom.bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.mdm_links.sh
+LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.modem_links.sh
+LOCAL_MODULE       := init.carrier.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.modem_links.sh
+LOCAL_SRC_FILES    := etc/init.carrier.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.coex.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.coex.sh
-include $(BUILD_PREBUILT)
+endif
