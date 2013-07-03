6d3393efe426324db98b41dfe9d8c1476913eb8c
lbdroid <definesinsanity@gmail.com>
2013-07-03 13:10:27 -0400

    Add apexq scripts
    Change-Id: I8f7db22b3b387dcf629c56fc41fa0ea0590ba6e8

---
+++ scripts/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := 50bluetooth
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := 50bluetooth
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := 60compass
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := 60compass
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := wifimac.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := wifimac.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT)/bin
+include $(BUILD_PREBUILT)
