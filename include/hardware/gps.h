27c5ab9b0f29f95c710be0336ccd17eb02a778e9
David Ferguson <ferguson.david@gmail.com>
2013-07-31 10:57:15 -0400

    msm8960: support Samsung extended AGPS
      * references:
          http://review.cyanogenmod.org/#/c/45564/
          http://review.cyanogenmod.org/#/c/45565/
    
    Change-Id: I815dc4155cb624f0a90ed7cff98a132baca2d13b

--- include/hardware/gps.h
+++ include/hardware/gps.h
@@ -280,16 +280,21 @@ typedef uint16_t AGpsStatusValue;
  * Name for the AGPS-RIL interface.
  */
 #define AGPS_RIL_INTERFACE      "agps_ril"
+
+/**
+ * The GPS chipset can use Psc for AGPS
+ */
+#define AGPS_USE_PSC
+
+/**
+ * Name for the GPS_Geofencing interface.
+ */
+#define GPS_GEOFENCING_INTERFACE   "gps_geofencing"
+
 /**
 * Name for ULP Phone Context Interface
 */
 #define ULP_PHONE_CONTEXT_INTERFACE "ulp-phone-context"
-
-/**
-* Name for the GPS_Geofencing interface.
-*/
-#define GPS_GEOFENCING_INTERFACE "gps_geofencing"
-
 
 /** Represents recurrence of location */
 typedef enum{
@@ -479,9 +484,8 @@ typedef struct {
     uint16_t mcc;
     uint16_t mnc;
     uint16_t lac;
-#if 1
-    /** Placeholder for Samsung ABI compat */
-    uint16_t unknown;
+#ifdef AGPS_USE_PSC
+    uint16_t psc;
 #endif
     uint32_t cid;
 } AGpsRefLocationCellID;
