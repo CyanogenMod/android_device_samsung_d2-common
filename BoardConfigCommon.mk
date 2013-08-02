cb6e9982c30a802580cb98198c01f767b218a8b0
Desterly <d3s7royer@gmail.com>
2013-08-02 19:50:04 -0400

    d2-common : Enable SELinux
    Change-Id: I95ce6702f6327f68f33db9b92691299669f69c24

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,3 +79,38 @@ ifneq ($(VARIENT_MODEL),apexqtmo)
 # Use MM heap for Camera
 BOARD_CAMERA_USE_MM_HEAP := true
 endif
+
+# SELinux
+ifeq ($(HAVE_SELINUX),true)
+BOARD_SEPOLICY_DIRS += \
+        device/samsung/d2-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+file_contexts \
+property_contexts \
+te_macros \
+bridge.te \
+camera.te \
+conn_init.te \
+device.te \
+dhcp.te \
+domain.te \
+drmserver.te \
+file.te \
+kickstart.te \
+init.te \
+mediaserver.te \
+mpdecision.te \
+netmgrd.te \
+property.te \
+qmux.te \
+rild.te \
+rmt.te \
+sensors.te \
+surfaceflinger.te \
+system.te \
+tee.te \
+thermald.te \
+ueventd.te \
+wpa_supplicant.te
+endif
