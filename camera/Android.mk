e03372e66036d8463244ada6ec3ea0c13826198f
jt1134 <jt1134@gmail.com>
2013-08-19 21:43:57 -0500

    Move some stuff from apextmo/expressatt to common
    - Avoid some code duplication.
    - Move camera wrapper from apexq (modify it to fix ISO modes) and
      make it available for other devices (ie: expressatt). Include
      media_profiles.xml for isx012.
    - Include some additional scripts.
    - Requires some slight changes to device trees.
    
    Change-Id: Idfbca694274e2a8edd747e6a6aa9960ad37cd07f

---
+++ camera/Android.mk
@@ -0,0 +1,19 @@
+# Currently only for apexqtmo and expressatt
+ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.MSM8960
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
--- scripts/Android.mk
+++ scripts/Android.mk
@@ -18,6 +18,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := 60sensors
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := 60sensors
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := wifimac.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
