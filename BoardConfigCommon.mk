15ba93162d3c03ecaf4ed19778fac598d6a6c5e2
Dan Pasanen <dan.pasanen@gmail.com>
2013-12-11 19:19:49 -0600

    Camera: enable TARGET_NEED_CAMERA_ZSL
    Change-Id: Ifcb18dc9cf9238aa53627edb886d8cc3f206a630

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,6 +72,9 @@ BOARD_HAVE_DOCK_USBAUDIO := true
 #camera abi compatiblily
 TARGET_DISPLAY_INSECURE_MM_HEAP := true
 COMMON_GLOBAL_CFLAGS += -DQCOM_BSP_CAMERA_ABI_HACK
+ifeq ($(filter apexqtmo expressatt,$(VARIENT_MODEL)),)
+  TARGET_NEED_CAMERA_ZSL := true
+endif
 
 # Needed for blobs
 COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
