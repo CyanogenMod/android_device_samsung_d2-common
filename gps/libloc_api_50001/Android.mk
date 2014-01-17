a21ecbf1974a7c7e0024206806ce6045bc449a19
Shareef Ali <shareefalis@cyanogenmod.org>
2014-01-16 20:44:16 -0600

    d2lte: make tree universal.
    * do what hammerhead did.
    * shove cdma and gsm into one universal tree
    * add variant script based on attn1's jfgsm script
    
    Change-Id: I4f2063ad5296297a945976ca011823c2a0487129

--- gps/libloc_api_50001/Android.mk
+++ gps/libloc_api_50001/Android.mk
@@ -86,7 +86,7 @@ LOCAL_CFLAGS += \
 
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
-    device/samsung/d2-common/gps/ulp/inc
+    device/samsung/d2lte/gps/ulp/inc
 
 LOCAL_PRELINK_MODULE := false
 
@@ -118,7 +118,7 @@ LOCAL_CFLAGS += \
 ## Includes
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
-    device/samsung/d2-common/gps/ulp/inc
+    device/samsung/d2lte/gps/ulp/inc
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
