63642bfd24acad2b4fd1d2c0cbbda650e81a4a37
Dave Daynard <nardholio@gmail.com>
2014-01-18 22:34:30 -0500

    d2-common: dex-opt to cache
    Cache partition is ~850 MB. We have the space.
    
    Change-Id: I70e0bda7553b6c2dab4aed97feda8b817a9ef3b9
    (cherry picked from commit 4e9605f5a3b958715ab14a6c921ec8c4f151cbac)

--- d2-common.mk
+++ d2-common.mk
@@ -134,7 +134,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_short_ons_0="" \
     persist.rild.nitz_short_ons_1="" \
     persist.rild.nitz_short_ons_2="" \
-    persist.rild.nitz_short_ons_3=""
+    persist.rild.nitz_short_ons_3="" \
+    dalvik.vm.dexopt-data-only=0
 
 ifneq ($(VARIENT_MODEL),apexqtmo)
 PRODUCT_PROPERTY_OVERRIDES += \
