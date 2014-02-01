df2beb983896afd3e77b6399e80d0d97fb535ba2
Dan Pasanen <dan.pasanen@gmail.com>
2014-02-01 10:20:21 -0600

    variant fixes
    * get rid of variant.sh
    * utilize INIT_VENDOR_LIB for our variant props
    
    Change-Id: I9eee2e1239a07dee2fc13481264646c16b1cef83

--- cm.mk
+++ cm.mk
@@ -6,12 +6,4 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=d2uc \
-    TARGET_DEVICE=d2lte \
-    BUILD_FINGERPRINT="xxxxxxxxxx" \
-    PRIVATE_BUILD_DESC="xxxxxxxxxx"
-
 PRODUCT_NAME := cm_d2lte
-PRODUCT_DEVICE := d2lte
-
