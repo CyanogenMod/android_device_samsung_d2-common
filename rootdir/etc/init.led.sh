cf315bc2a993a6c3463afecbd602dec8fb47c4b4
Jon S <ajrty33@gmail.com>
2014-02-11 19:22:54 +0000

    d2: prevent boot led from overriding charging led, take 2
    ACTION!
    
    Change-Id: If85d326b2efc64e0bc426fce197ff1b4a3b9911e

--- rootdir/etc/init.led.sh
+++ rootdir/etc/init.led.sh
@@ -1,8 +1,7 @@
 #!/system/bin/sh
 
 # prevent boot led from overriding charging led
-ledstatus = `cat /sys/class/sec/led/led_pattern`
-if [ ledstatus == "6" ]
-then
-echo 0 > /sys/class/sec/led/led_pattern
+LEDSTATUS = `cat /sys/class/sec/led/led_pattern`
+if [ $LEDSTATUS = "6" ]; then
+    echo 0 > /sys/class/sec/led/led_pattern
 fi
