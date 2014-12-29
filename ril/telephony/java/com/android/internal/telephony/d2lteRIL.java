e9fe57fdb30168e9d48b5a3a7c5a4103d6f5e499
dcd <dcd1182@gmail.com>
2014-12-28 22:36:14 -0600

    d2: ril: remove lollipop hack
    instead use http://review.cyanogenmod.org/#/c/83032/
    
    Change-Id: I2bae26ed6122af48d95c317628733ada003a71fe

--- ril/telephony/java/com/android/internal/telephony/d2lteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/d2lteRIL.java
@@ -779,31 +779,6 @@ public class d2lteRIL extends RIL implements CommandsInterface {
         send(rr);
     }
 
-    // Hack for Lollipop
-    // The system now queries for SIM status before radio on, resulting
-    // in getting an APPSTATE_DETECTED state. The RIL does not send an
-    // RIL_UNSOL_RESPONSE_SIM_STATUS_CHANGED message after the SIM is
-    // initialized, so delay the message until the radio is on.
-    @Override
-    public void
-    getIccCardStatus(Message result) {
-        if (mState != RadioState.RADIO_ON) {
-            mPendingGetSimStatus = result;
-        } else {
-            super.getIccCardStatus(result);
-        }
-    }
-
-    @Override
-    protected void switchToRadioState(RadioState newState) {
-        super.switchToRadioState(newState);
-
-        if (newState == RadioState.RADIO_ON && mPendingGetSimStatus != null) {
-            super.getIccCardStatus(mPendingGetSimStatus);
-            mPendingGetSimStatus = null;
-        }
-    }
-
     // This call causes ril to crash the socket, stopping further communication
     @Override
     public void
