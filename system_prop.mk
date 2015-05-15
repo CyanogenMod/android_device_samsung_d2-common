0a288626ebb94a12f010858ec43278a97411428a
Dave Daynard <nardholio@gmail.com>
2015-05-14 20:18:36 -0400

    d2: borrow serrano's audio policy
    Change-Id: I8ec97852d26d835f39892ca41d3c28f18fe2d480

--- system_prop.mk
+++ system_prop.mk
@@ -3,6 +3,7 @@
 
 # audio
 PRODUCT_PROPERTY_OVERRIDES += \
+    audio.offload.disable=1 \
     persist.audio.fluence.mode=endfire \
     persist.audio.handset.mic=digital \
     ro.qc.sdk.audio.fluencetype=fluence
