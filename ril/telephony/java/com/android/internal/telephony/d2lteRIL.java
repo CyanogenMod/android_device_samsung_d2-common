35a6fb94d76ad8e9abb44c264749295699df3e04
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-25 23:56:14 -0600

    d2lteRIL: remove setting mQANElements
    * this is handled via the prop in the super class now
    
    Change-Id: I83318419d3fec38cd06444c68efa806a634ae126

--- ril/telephony/java/com/android/internal/telephony/d2lteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/d2lteRIL.java
@@ -53,14 +53,12 @@ public class d2lteRIL extends RIL implements CommandsInterface {
     public d2lteRIL(Context context, int networkModes, int cdmaSubscription) {
         this(context, networkModes, cdmaSubscription, null);
         mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
-        mQANElements = SystemProperties.getInt("ro.ril.telephony.mqanelements", 4);
     }
 
     public d2lteRIL(Context context, int preferredNetworkType,
             int cdmaSubscription, Integer instanceId) {
         super(context, preferredNetworkType, cdmaSubscription, instanceId);
         mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
-        mQANElements = SystemProperties.getInt("ro.ril.telephony.mqanelements", 4);
     }
 
     @Override
