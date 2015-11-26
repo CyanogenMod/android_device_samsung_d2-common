b3a56cd23f49c36f942717468e90275a17f5e319
Dan Pasanen <dan.pasanen@gmail.com>
2015-11-25 22:42:47 -0600

    d2lteRIL: remove sms hax
    Change-Id: I45316343899d243740df75ff1288d2b681195d12

--- ril/telephony/java/com/android/internal/telephony/d2lteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/d2lteRIL.java
@@ -45,10 +45,7 @@ public class d2lteRIL extends RIL implements CommandsInterface {
 
     private AudioManager mAudioManager;
     private boolean isGSM = false;
-    private boolean mIsSendingSMS = false;
     private static final int RIL_REQUEST_DIAL_EMERGENCY = 10001;
-    public static final long SEND_SMS_TIMEOUT_IN_MS = 30000;
-    private Object mSMSLock = new Object();
 
     public d2lteRIL(Context context, int networkModes, int cdmaSubscription) {
         this(context, networkModes, cdmaSubscription, null);
@@ -131,44 +128,6 @@ public class d2lteRIL extends RIL implements CommandsInterface {
             cardStatus.mApplications[cardStatus.mImsSubscriptionAppIndex] = appStatus3;
         }
         return cardStatus;
-    }
-
-    @Override
-    public void
-    sendCdmaSms(byte[] pdu, Message result) {
-        smsLock();
-        super.sendCdmaSms(pdu, result);
-    }
-
-    @Override
-    public void
-        sendSMS (String smscPDU, String pdu, Message result) {
-        smsLock();
-        super.sendSMS(smscPDU, pdu, result);
-    }
-
-    private void smsLock(){
-        // Do not send a new SMS until the response for the previous SMS has been received
-        //   * for the error case where the response never comes back, time out after
-        //     30 seconds and just try the next SEND_SMS
-        synchronized (mSMSLock) {
-            long timeoutTime  = SystemClock.elapsedRealtime() + SEND_SMS_TIMEOUT_IN_MS;
-            long waitTimeLeft = SEND_SMS_TIMEOUT_IN_MS;
-            while (mIsSendingSMS && (waitTimeLeft > 0)) {
-                Rlog.d(RILJ_LOG_TAG, "sendSMS() waiting for response of previous SEND_SMS");
-                try {
-                    mSMSLock.wait(waitTimeLeft);
-                } catch (InterruptedException ex) {
-                    // ignore the interrupt and rewait for the remainder
-                }
-                waitTimeLeft = timeoutTime - SystemClock.elapsedRealtime();
-            }
-            if (waitTimeLeft <= 0) {
-                Rlog.e(RILJ_LOG_TAG, "sendSms() timed out waiting for response of previous CDMA_SEND_SMS");
-            }
-            mIsSendingSMS = true;
-        }
-
     }
 
     @Override
@@ -520,20 +479,6 @@ public class d2lteRIL extends RIL implements CommandsInterface {
         }
 
         super.notifyRegistrantsCdmaInfoRec(infoRec);
-    }
-
-
-
-    @Override
-    protected Object
-    responseSMS(Parcel p) {
-        // Notify that sendSMS() can send the next SMS
-        synchronized (mSMSLock) {
-            mIsSendingSMS = false;
-            mSMSLock.notify();
-        }
-
-        return super.responseSMS(p);
     }
 
     @Override
