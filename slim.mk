28464446b2563b21850fa94fcc348cc8a3803fab
Perseus71 <perseus71@gmail.com>
2015-12-21 04:53:19 +0100

    Bring up slim.
    Change-Id: I39da5931ade642f5a5b5037d6839eafb8ae3ee30

--- slim.mk
+++ slim.mk
@@ -3,5 +3,5 @@ $(call inherit-product, device/samsung/d2-common/d2-common.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
