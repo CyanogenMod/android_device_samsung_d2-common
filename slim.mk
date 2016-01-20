5d5dd5aa0c53308b649ae16e7653dd76635269aa
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-01-20 06:16:39 -0600

    d2: update gps build flag
    Change-Id: I50694cf36a919b49821e404d09d323dda2f30238

---
+++ slim.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/d2-common/d2-common.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
