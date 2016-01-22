2200a37af608f55fb7961120b575612f34ed9835
Sanket Padawe <sanketpadawe@google.com>
2016-01-21 18:55:25 -0800

    d2: ril: Update RIL wakelock symantics
    Squash the needed 7.0 RIL updates from framework/opt/telephony:
    
    060a6f0ba56dde532a9b9b28d4302df86a556691 Improve wakelock symantics
    a05275d90833d2ff26a2571e2cc0f2ea91fd2b43 Additions to ril wakelock symantics.
    
    Change-Id: I8704fc30c3078c895c9e20a2b51b30cad760d7b2

--- ril/telephony/java/com/android/internal/telephony/d2lteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/d2lteRIL.java
@@ -32,6 +32,7 @@ import com.android.internal.telephony.cdma.CdmaInformationRecords;
 import com.android.internal.telephony.cdma.SignalToneUtil;
 import com.android.internal.telephony.uicc.IccCardApplicationStatus;
 import com.android.internal.telephony.uicc.IccCardStatus;
+import com.android.internal.telephony.uicc.IccUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -241,7 +242,7 @@ public class d2lteRIL extends RIL implements CommandsInterface {
 
     @Override
     protected void
-    processUnsolicited (Parcel p) {
+    processUnsolicited (Parcel p, int type) {
         Object ret;
         int dataPosition = p.dataPosition(); // save off position within the Parcel
         int response = p.readInt();
@@ -284,7 +285,7 @@ public class d2lteRIL extends RIL implements CommandsInterface {
                 p.setDataPosition(dataPosition);
 
                 // Forward responses that we are not overriding to the super class
-                super.processUnsolicited(p);
+                super.processUnsolicited(p, type);
                 return;
         }
 
@@ -292,7 +293,7 @@ public class d2lteRIL extends RIL implements CommandsInterface {
 
     @Override
     protected RILRequest
-    processSolicited (Parcel p) {
+    processSolicited (Parcel p, int type) {
         int serial, error;
         boolean found = false;
         int dataPosition = p.dataPosition(); // save off position within the Parcel
@@ -326,7 +327,7 @@ public class d2lteRIL extends RIL implements CommandsInterface {
             /* Nothing we care about, go up */
             p.setDataPosition(dataPosition);
             // Forward responses that we are not overriding to the super class
-            return super.processSolicited(p);
+            return super.processSolicited(p, type);
         }
         rr = findAndRemoveRequestFromList(serial);
         if (rr == null) {
