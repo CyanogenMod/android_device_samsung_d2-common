c812e14a1fa223a79a8a60f229df5851b7938a0b
Steve Kondik <steve@cyngn.com>
2016-09-07 09:10:54 -0500

    d2: Turn on sdcardfs
     * Ditch shitty FUSE for the new hotness
    
    Change-Id: Id24fe7ff556ae50546abb9fb131320d2612d4131

--- system_prop.mk
+++ system_prop.mk
@@ -25,3 +25,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.zsl.prop.enable=0
+
+# storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
