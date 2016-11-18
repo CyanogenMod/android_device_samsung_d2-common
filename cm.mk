eacbb3014d35c0a57238bf0f1d9de5975bf33725
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-18 03:36:00 -0600

    d2-common: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8

--- cm.mk
+++ cm.mk
@@ -1,7 +1,4 @@
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
